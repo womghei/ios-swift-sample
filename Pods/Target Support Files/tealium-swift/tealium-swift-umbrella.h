4b4415485885afa9971db09faf28a889fab0da70
womghei <womghei@connect.hku.hk>
2020-01-21 18:08:11 +0800

    initial commit

---
+++ Pods/Target Support Files/tealium-swift/tealium-swift-umbrella.h
@@ -0,0 +1,19 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "Disk.h"
+#import "DiskErrors.h"
+#import "diskstorage-Bridging-Header.h"
+
+FOUNDATION_EXPORT double TealiumSwiftVersionNumber;
+FOUNDATION_EXPORT const unsigned char TealiumSwiftVersionString[];
+
